52de43534e941c71fe7fbb019b05346a9397b15d
Mar <marbou96@hotmail.com>
2025-11-17 17:02:30 +0100

    configuración inicial

---
+++ src/test/java/com/BookTracker/book_tracker/BookTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BookTracker.book_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
